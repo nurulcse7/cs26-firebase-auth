4ff86fe975e9e64e1bea24354732715b4a368196
nurulcse7 <nurul.cse7@gmail.com>
2022-10-16 21:44:11 +0600

    Added type writer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <Typewriter
+        options={{
+          strings: [
+            `<h3>Hello I'm</h3>
+            <h1>Nurul Islam</h1>
+            <h2 className="text-light">Fullstack Web and Apps Developer</h2>`
+          ],
+          autoStart: true,
+          loop: true,
+        }}
+      />
     </div>
   );
 };
